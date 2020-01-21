9692d1e77885d4e81fdddd2aaccd432cd6e70c26
dgoleniewski <goleniewski.damian1@gmail.com>
2020-01-21 15:44:57 +0100

    Ksiegarnia

---
+++ Ksiegarnia/Data/BookContext.cs
@@ -0,0 +1,19 @@
+﻿using Ksiegarnia.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ksiegarnia.Data
+{
+    //klasa kontekstu bazy danych
+    public class BookContext : DbContext
+    {
+        public BookContext (DbContextOptions<BookContext> options) : base(options)
+        {
+
+        }
+        public DbSet<BookModel> Book { get; set; }
+    }
+}
